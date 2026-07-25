68729b0d241b8cb3f1a99c427f636ef43f67acfb
Mónica <morodriguez112@gmail.com>
2026-07-24 19:25:01 -0500

    Primer commit proyecto Spring Boot Easy Parking

---
+++ src/main/java/com/proyectofinaleasyparking/proyectofinaleasyparking/ProyectofinaleasyparkingApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofinaleasyparking.proyectofinaleasyparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectofinaleasyparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectofinaleasyparkingApplication.class, args);
+	}
+
+}
